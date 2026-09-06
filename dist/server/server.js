2c50bff3e86b42a47ca0649b48a17838634f72ea
D Sai Pranav <dsaipranav3125@gmail.com>
2026-09-06 17:07:13 +0530

    feat: add About page, site footer, and server-side error handling infrastructure

--- dist/server/server.js
+++ dist/server/server.js
@@ -57,7 +57,7 @@ function renderErrorPage() {
 //#region src/server.ts
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-D1lRMl4N.js").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-CbbdiG0n.js").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
