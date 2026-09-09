1ac65ee08e8c1ff740de339bc12ee8f196d96da6
D Sai Pranav <dsaipranav3125@gmail.com>
2026-09-09 16:14:54 +0530

    feat: add product database and associated build artifacts

--- dist/server/server.js
+++ dist/server/server.js
@@ -57,7 +57,7 @@ function renderErrorPage() {
 //#region src/server.ts
 var serverEntryPromise;
 async function getServerEntry() {
-	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-BPCRHrv7.js").then((m) => m.default ?? m);
+	if (!serverEntryPromise) serverEntryPromise = import("./assets/server-iUt1klik.js").then((m) => m.default ?? m);
 	return serverEntryPromise;
 }
 async function normalizeCatastrophicSsrResponse(response) {
